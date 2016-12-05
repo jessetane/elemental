534cead4e419c58cad1cddf4e02042d2208ceaf9
Danara Sarioglu <ds4489@nyu.edu>
2016-12-05 12:46:02 -0500

    insertChildAtIndex added

--- index.js
+++ index.js
@@ -44,3 +44,12 @@ Element.prototype.bind = function () {
     this[method] = this[method].bind(this)
   }
 }
+
+Element.prototype.insertChildAtIndex = function(child, index) {
+  if (!index) index = 0
+  if (index >= this.children.length) {
+    this.appendChild(child)
+  } else {
+    this.insertBefore(child, this.children[index])
+  }
+}
